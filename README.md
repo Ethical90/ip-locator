44b4b1b0f41463a07e35e74abe471abc599e9dda
Ethical Rufai <34621845+Ethical90@users.noreply.github.com>
2019-07-08 00:41:02 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+# ip-locator
+# python 2.x
+# version 1.0
+# For Educational purposes only. 
+
+command: git clone https://github.com/Ethical90/ip-locator.git 
+
+# [+]Country name
+# [+]Region name
+# [+]City name
+# [+]Latitude
+# [+]Longitude
+# [+]ISP name
+
+
+
+In code:
+
+main_api = ('https://api.ip2location.com/?ip='+str(ip_address)+'&key=*****&package=WS6') #replace ****** with your API key
+
+You can sign up for a trial key that enable you to test the REST API for up to 5,000 credits.
+https://www.ip2location.com/web-service
+
+Enter IP:45.56.155.206
+********************************************************************************
+                              Created by Ethical                               
+                                  Version 1.0                                   
+********************************************************************************
+
+[*] Tracking 45.56.155.206 for credentials...
+
+
---
+++ ip-locator.py
@@ -0,0 +1,39 @@
+import requests
+import socket
+import sys
+import urllib
+import pyperclip
+from datetime import datetime
+try:
+	ip_address=raw_input('Enter IP:')
+	main_api = ('https://api.ip2location.com/?ip='+str(ip_address)+'&key=******&package=WS6') #replace api with yours
+	response = requests.get(main_api)
+#banner design
+  	print('*'*80)
+  	print('Created by Ethical').center(80)
+	print('Version 1.0').center(80)
+  	print('*'*80)
+	t1=datetime.now()	
+	if response.status_code==200:
+		print('\n[*] Tracking '+str(ip_address)+' for credentials...')
+		browser=urllib.urlopen(main_api)
+		page=browser.read()
+		pyperclip.copy(page)
+		paste=pyperclip.paste()
+		line=paste.split(';')
+		print('[+]Country name: '+line[1])
+		print('[+]Region name: '+line[2])
+		print('[+]City name: '+line[3])
+		print('[+]Latitude: '+line[4])
+		print('[+]Longitude: '+line[5])
+		print('[+]ISP name: '+line[6])
+		t2=datetime.now()
+		total=(t2-t1)
+		print('Scan Completed in: ' + str(total))	
+	else:	
+		timeout=10
+		socket.setdefaulttimeout(timeout)
+		print('Connection timeout')
+
+except KeyboardInterrupt:
+	sys.exit()
